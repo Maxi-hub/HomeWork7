2e77839c517b0c777a89cfaeea3d2b73ce59efd2
Emma <em.lebedeva85@gmail.com>
2023-06-13 15:01:57 +0300

    Create Task50, Task52 & add .gitignore

---
+++ Task50/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1,7] -> такого числа в массиве нет
+
+
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetArray2d()
+{
+    int m = Prompt("Введите значение m");
+    int n = Prompt("Введите значение n");
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }        
+    }
+
+    return array;
+}
+
+void PrintArray2d(int[,] array2d)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            Console.Write(array2d[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = GetArray2d();
+PrintArray2d(array2d);
+
+void GetNumber(int[,] array)
+{
+    int m1 = Prompt("Введите номер строки");
+    int n1 = Prompt("Введите номер столбца");
+    int number = array2d[m1, n1];
+    if(m1 <= array2d.GetLength(0) && n1 <= array2d.GetLength(1))
+    { 
+        Console.WriteLine($"Значение ячейки: {number}"); 
+    }
+    else 
+    {
+        Console.WriteLine("Такой ячейки не существует");
+    }
+}
+
+GetNumber(array2d);
+
---
+++ Task52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetArray2d()
+{
+    int m = Prompt("Введите значение m");
+    int n = Prompt("Введите значение n");
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }        
+    }
+
+    return array;
+}
+
+void PrintArray2d(int[,] array2d)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            Console.Write(array2d[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = GetArray2d();
+PrintArray2d(array2d);
+
+
+void Average(int[,] array)
+{
+    double[] averages = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {       
+            sum += array[j, i];
+        }
+        averages[i] = sum / array.GetLength(0);
+        Console.WriteLine("Среднее арифметическое значение элементов столбца {0}: {1}", i + 1, Math.Round(averages[i], 2));  
+    // вместо {0} подставляется значение i+1, а вместо {1} следующее значение после запятой 
+    }
+    
+}
+    
+Average(array2d);
+   
